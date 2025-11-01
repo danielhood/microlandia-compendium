b5b6916166244c76a13c6953a4e99e713bd96210
danielhood <danielhood@users.noreply.github.com>
2025-11-01 15:32:14 -0400

    Added header animation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,6 +7,18 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   imports: [RouterOutlet, RouterLink],
   template: `
     <header>
+      <div class="micro-bg" aria-hidden="true">
+        <span class="micro m1"></span>
+        <span class="micro m2"></span>
+        <span class="micro m3"></span>
+        <span class="micro m4"></span>
+        <span class="micro m5"></span>
+        <span class="micro m6"></span>
+        <span class="micro m7"></span>
+        <span class="micro m8"></span>
+        <span class="micro m9"></span>
+        <span class="micro m10"></span>
+      </div>
       <h1>Microlandia Compendium</h1>
       <nav>
         <a routerLink="/">Observations</a>
@@ -19,4 +31,3 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   `
 })
 export class AppComponent {}
-
