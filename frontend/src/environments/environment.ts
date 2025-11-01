70ec05f83c2060b940c37a6a793a37fc479d4b61
danielhood <danielhood@users.noreply.github.com>
2025-11-01 18:56:34 -0400

    Added AWS hosting config

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   // Point this to your API base URL in dev
   apiBaseUrl: 'http://10.10.11.107:8080/api',
   // URL used for generating QR code in the UI
-  shareQrUrl: 'http://localhost:4200/'
+  shareQrUrl: 'http://microlandia-compendium.s3-website.ca-central-1.amazonaws.com'
 };
